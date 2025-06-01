c6e261c084b36e023d0a8e19473a6ff9275526e5
Jakhongir <ganiyevuzb@gmail.com>
2025-06-01 17:03:37 +0500

    🚀 feat: simplify swagger.py by removing unused imports and dynamic description function

--- conf/swagger.py
+++ conf/swagger.py
@@ -1,28 +1,9 @@
 from django.urls import path
 
-from rest_framework.permissions import AllowAny
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
-from conf.settings import API_VERSION, BACKEND_DOMAIN
-
-
-def get_dynamic_description(domain) -> str:
-    """
-    Get the API description based on the subdomain.
-    """
-    host = domain.split(':')[0]
-    subdomain = host.split('.')[0]
-    if subdomain == "test":
-        return "Development API"
-    elif subdomain == "api":
-        return "Production API"
-    else:
-        return "Generic API"
-
-
 
 # Add `never_cache` to prevent caching of the schema view
 urlpatterns = [
